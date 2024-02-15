7291ce063b564b6256c46a380f5612bd1498e29f
Nolami <deata7660@gmail.com>
2024-02-15 12:14:08 +0200

    Update need.js

--- need.js
+++ need.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", loggedIn)
 
 function loggedIn()
  {
-    let logged = "yes"
-    if (logged === "yes"){
+    if (localStorage.getItem("logged") === "true"){
         document.getElementById("text ").textContent += localStorage.getItem("name")
+        document.getElementById("name").style.display = "none"
     }
  }
  function login() {
     localStorage.setItem("name", document.getElementById("name". value))
+    localStorage.setItem("logged", "true")
  }
